af457909800ae5402b3268e377765a3738755769
drunk bois developor <105758302+drunkboisdev@users.noreply.github.com>
2022-09-20 17:40:59 -0400

    fix input (for real this time)

--- script.js
+++ script.js
@@ -5,13 +5,18 @@ let sprite = PIXI.Sprite.from('placeholder0.png')
 app.stage.addChild(sprite)
 
 window.addEventListener("keydown", (e) => {
+    // there's probably a better way to do this
     if (e.key === "ArrowRight" || e.key === "d") {
+        app.stage.removeChild(sprite)
         sprite = PIXI.Sprite.from('placeholder3.png')
     } else if (e.key === "ArrowDown" || e.key === "s") {
+        app.stage.removeChild(sprite)
         sprite = PIXI.Sprite.from('placeholder2.png')
     } else if (e.key === "ArrowLeft" || e.key === "a") {
+        app.stage.removeChild(sprite)
         sprite = PIXI.Sprite.from('placeholder1.png')
     } else if (e.key === "ArrowUp" || e.key === "w") {
+        app.stage.removeChild(sprite)
         sprite = PIXI.Sprite.from('placeholder0.png')
     }
     app.stage.addChild(sprite)
