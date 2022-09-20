402218aeb4763dcc76ee8917072622e335c7d297
drunk bois developor <105758302+drunkboisdev@users.noreply.github.com>
2022-09-20 17:20:34 -0400

    Add keyboard input

--- script.js
+++ script.js
@@ -5,7 +5,15 @@ let sprite = PIXI.Sprite.from('placeholder0.png')
 app.stage.addChild(sprite)
 
 window.addEventListener("keydown", (e) => {
-    console.log(e.key)
+    if (e.key === "ArrowRight" || e.key === "d") {
+        sprite = PIXI.Sprite.from('placeholder3.png')
+    } else if (e.key === "ArrowDown" || e.key === "s") {
+        sprite = PIXI.Sprite.from('placeholder3.png')
+    } else if (e.key === "ArrowLeft" || e.key === "a") {
+        sprite = PIXI.Sprite.from('placeholder3.png')
+    } else if (e.key === "ArrowUp" || e.key === "w") {
+        sprite = PIXI.Sprite.from('placeholder3.png')
+    }
 })
 
 let elapsed = 0.0
