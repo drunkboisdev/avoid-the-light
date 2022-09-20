9aca830d2866abe5e74fc0d96990af8c37016177
drunk bois developor <105758302+drunkboisdev@users.noreply.github.com>
2022-09-20 19:04:30 -0400

    swap vertical movement keys

--- script.js
+++ script.js
@@ -12,16 +12,17 @@ window.addEventListener("keydown", (e) => {
     if (e.key === "ArrowRight" || e.key === "d") {
         app.stage.removeChild(sprite)
         sprite = PIXI.Sprite.from('placeholder3.png')
-    } else if (e.key === "ArrowDown" || e.key === "s") {
+    } else if (e.key === "ArrowUp" || e.key === "w") {
         app.stage.removeChild(sprite)
         sprite = PIXI.Sprite.from('placeholder2.png')
     } else if (e.key === "ArrowLeft" || e.key === "a") {
         app.stage.removeChild(sprite)
         sprite = PIXI.Sprite.from('placeholder1.png')
-    } else if (e.key === "ArrowUp" || e.key === "w") {
+    } else if (e.key === "ArrowDown" || e.key === "s") {
         app.stage.removeChild(sprite)
         sprite = PIXI.Sprite.from('placeholder0.png')
     }
+    sprite.anchor.set(0.5)
     sprite.x = 640
     sprite.y = 360
     app.stage.addChild(sprite)
