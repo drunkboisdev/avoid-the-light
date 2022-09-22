95f097d12d5ae8cad691320079b0c69e598c3d17
drunk bois developor <105758302+drunkboisdev@users.noreply.github.com>
2022-09-21 20:51:25 -0400

    i give up
    expanded bg

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ document.body.appendChild(app.view)
 const bg = new PIXI.Container()
 app.stage.addChild(bg)
 
-for (let i = 0; i < 64; i++) {
-    for (let j = 0; j < 36; j++) {
+for (let i = -16; i < 80; i++) {
+    for (let j = -9; j < 45; j++) {
         const bgTile = new PIXI.Sprite.from('placeholder4.png');
         bgTile.x = i * 20;
         bgTile.y = j * 20;
@@ -25,42 +25,18 @@ window.addEventListener("keydown", (e) => {
     if (e.key === "ArrowRight" || e.key === "d") {
         app.stage.removeChild(sprite)
         sprite = PIXI.Sprite.from('placeholder3.png')
-        for (let i = 0; i < 36; i++) {
-            const bgTile = new PIXI.Sprite.from('placeholder4.png');
-            bgTile.x = app.width - 20;
-            bgTile.y = i * 20;
-            bg.addChild(bgTile);
-        }
         bg.x -= 20
     } else if (e.key === "ArrowUp" || e.key === "w") {
         app.stage.removeChild(sprite)
         sprite = PIXI.Sprite.from('placeholder2.png')
-        for (let i = 0; i < 64; i++) {
-            const bgTile = new PIXI.Sprite.from('placeholder4.png');
-            bgTile.x = i * 20;
-            bgTile.y = app.height - 20;
-            bg.addChild(bgTile);
-        }
         bg.y -= 20
     } else if (e.key === "ArrowLeft" || e.key === "a") {
         app.stage.removeChild(sprite)
         sprite = PIXI.Sprite.from('placeholder1.png')
-        for (let i = 0; i < 36; i++) {
-            const bgTile = new PIXI.Sprite.from('placeholder4.png');
-            bgTile.x = 0;
-            bgTile.y = i * 20;
-            bg.addChild(bgTile);
-        }
         bg.x += 20
     } else if (e.key === "ArrowDown" || e.key === "s") {
         app.stage.removeChild(sprite)
         sprite = PIXI.Sprite.from('placeholder0.png')
-        for (let i = 0; i < 64; i++) {
-            const bgTile = new PIXI.Sprite.from('placeholder4.png');
-            bgTile.x = i * 20;
-            bgTile.y = 0;
-            bg.addChild(bgTile);
-        }
         bg.y += 20
     }
     sprite.anchor.set(0.5)
