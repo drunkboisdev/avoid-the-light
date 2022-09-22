a9e790e4c262e0279209f4b322d82b77a5987c0c
drunk bois developor <105758302+drunkboisdev@users.noreply.github.com>
2022-09-21 20:35:13 -0400

    revert movement

--- script.js
+++ script.js
@@ -19,40 +19,25 @@ sprite.x = 640
 sprite.y = 360
 app.stage.addChild(sprite)
 
-let time = 0
-
-function animateBG(speed, direction) {
-    if (time < 20) {
-        requestAnimationFrame(animateBG(speed, direction))
-    }
-    time++
-
-    if (direction === "x") {
-        bg.x += speed
-    } else if (direction === "y") {
-        bg.y += speed
-    }
-}
-
 window.addEventListener("keydown", (e) => {
     let time = 0
     // there's probably a better way to do this
     if (e.key === "ArrowRight" || e.key === "d") {
         app.stage.removeChild(sprite)
         sprite = PIXI.Sprite.from('placeholder3.png')
-        animateBG(-1, "x")
+        bg.x -= 20
     } else if (e.key === "ArrowUp" || e.key === "w") {
         app.stage.removeChild(sprite)
         sprite = PIXI.Sprite.from('placeholder2.png')
-        animateBG(-1, "y")
+        bg.y -= 20
     } else if (e.key === "ArrowLeft" || e.key === "a") {
         app.stage.removeChild(sprite)
         sprite = PIXI.Sprite.from('placeholder1.png')
-        animateBG(1, "x")
+        bg.x += 20
     } else if (e.key === "ArrowDown" || e.key === "s") {
         app.stage.removeChild(sprite)
         sprite = PIXI.Sprite.from('placeholder0.png')
-        animateBG(1, "y")
+        bg.y += 20
     }
     sprite.anchor.set(0.5)
     sprite.x = 640
